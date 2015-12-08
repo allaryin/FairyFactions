8bae715d29e5c551da8e191408ad5090bdd60c3e
Ammon Lauritzen <allaryin@gmail.com>
2015-12-08 13:47:41 -0600

    Actually register the renderers :)

--- src/main/java/fairies/proxy/ClientProxy.java
+++ src/main/java/fairies/proxy/ClientProxy.java
@@ -1,5 +1,33 @@
 package fairies.proxy;
 
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.relauncher.Side;
+import cpw.mods.fml.relauncher.SideOnly;
+import fairies.client.render.ModelFairy;
+import fairies.client.render.RenderFairy;
+import fairies.client.render.RenderFish;
+import fairies.entity.EntityFairy;
+import fairies.entity.FairyEntityFishHook;
+
+@SideOnly(Side.CLIENT)
 public class ClientProxy extends CommonProxy {
+	
+	@Override
+	public void preInit() {
+		super.preInit();
+		
+		initRenderer();
+	}
+	
+	private void initRenderer() {
+	    // map.put(FRY_EntityFairy.class, new FRY_RenderFairy(new FRY_ModelFairy(), 0.25F));
+	    // map.put(FRY_EntityFishHook.class, new FRY_RenderFish());
+		
+		RenderingRegistry.registerEntityRenderingHandler(EntityFairy.class, 
+				new RenderFairy(new ModelFairy(), 0.25f));
+		RenderingRegistry.registerEntityRenderingHandler(FairyEntityFishHook.class, new RenderFish());
+	}
+	
+
 
 }
