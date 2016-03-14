1b415df928318c006ad0e955ad671ef8fe504aea
Ammon Lauritzen <allaryin@gmail.com>
2016-03-14 12:30:56 -0500

    Nerfed wander speed.

--- src/main/java/fairies/entity/EntityFairy.java
+++ src/main/java/fairies/entity/EntityFairy.java
@@ -124,7 +124,7 @@ public class EntityFairy extends EntityAnimal {
         this.tasks.addTask(4, new EntityAIAttackOnCollide(this, 1.0D, true));
         // this.tasks.addTask(5, new EntityAIFollowOwner(this, 1.0D, 10.0F, 2.0F));
         // this.tasks.addTask(6, new EntityAIMate(this, 1.0D));
-        this.tasks.addTask(7, new EntityAIWander(this, 1.0D));
+        this.tasks.addTask(7, new EntityAIWander(this, 0.25D));
         // this.tasks.addTask(8, new EntityAIBeg(this, 8.0F));
         this.tasks.addTask(9, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
         this.tasks.addTask(9, new EntityAILookIdle(this));
