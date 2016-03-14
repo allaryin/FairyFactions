1a4e5e70ed681a5e23fe2d5410a335875b88f51d
Ammon Lauritzen <allaryin@gmail.com>
2016-03-14 12:26:43 -0500

    Stubs for base AI behaviors.

---
+++ src/main/java/fairies/ai/FairyAIPosted.java
@@ -0,0 +1,13 @@
+package fairies.ai;
+
+import net.minecraft.entity.ai.EntityAIBase;
+
+public class FairyAIPosted extends EntityAIBase {
+
+	@Override
+	public boolean shouldExecute() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ src/main/java/fairies/ai/FairyAIRogue.java
@@ -0,0 +1,13 @@
+package fairies.ai;
+
+import net.minecraft.entity.ai.EntityAIBase;
+
+public class FairyAIRogue extends EntityAIBase {
+
+	@Override
+	public boolean shouldExecute() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
